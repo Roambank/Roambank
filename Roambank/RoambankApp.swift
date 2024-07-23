fcfc6762987fdf9299351c748e3758e6cdd94bcb
tavilutvie <tavirazin@gmail.com>
2024-07-23 10:05:59 +0700

    Initial Commit

---
+++ Roambank/RoambankApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoambankApp.swift
+//  Roambank
+//
+//  Created by Eldenabih Tavirazin Lutvie on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RoambankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
