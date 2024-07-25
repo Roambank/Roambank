be747939c3b6ac349e57ce10d5208816a0a6effe
LisandraN08 <lnicoline@student.ciputra.ac.id>
2024-07-25 10:24:07 +0700

    feat: contributionview

--- Roambank/ViewModels/ContributionViewModel.swift
+++ Roambank/ViewModels/ContributionViewModel.swift
@@ -5,14 +5,55 @@
 //  Created by Lisandra Nicoline on 24/07/24.
 //
 
+import Foundation
+import Combine
 import SwiftUI
 
-struct ContributionViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class OrderViewModel: ObservableObject {
+    @Published var orders: [Order]
+    
+    init(orders: [Order] = []) {
+        self.orders = orders
+    }
+    
+    func addOrder(order: Order) {
+        orders.append(order)
     }
 }
 
-#Preview {
-    ContributionViewModel()
+class WasteViewModel: ObservableObject, Identifiable {
+    @Published var waste: Waste
+    
+    init(waste: Waste) {
+        self.waste = waste
+    }
+    
+    var id: UUID {
+        return waste.id
+    }
+    
+    var wasteType: WasteType {
+        return waste.wasteType
+    }
+    
+    var berat: Int {
+        get {
+            return waste.berat
+        }
+        set {
+            waste.berat = newValue
+        }
+    }
+}
+
+struct ContributionView_Previews: PreviewProvider {
+    static var previews: some View {
+        let wasteType = WasteType(nama: "Organic", gambar: "organic.png", poinPerKilo: 10, category: "Organic")
+        let wastes = [
+            Waste(wasteType: wasteType, berat: 5),
+            Waste(wasteType: wasteType, berat: 2)
+        ]
+        
+        return ContributionView(wastes: wastes)
+    }
 }
