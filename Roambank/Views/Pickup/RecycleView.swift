c0f16b058e4cd83c0510b1d4a09662887b002826
tavilutvie <tavirazin@gmail.com>
2024-07-26 10:05:33 +0700

    style: improve profile button and page view

--- Roambank/Views/Pickup/RecycleView.swift
+++ Roambank/Views/Pickup/RecycleView.swift
@@ -10,11 +10,14 @@ import SwiftUI
 struct RecycleView: View {
     @State private var navigateFromRecycle = false
     @State private var navigateToProfile = false
-
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(spacing: 20) {
+                    CustomHeaderView(navigateToProfile: $navigateToProfile)
+                    //                        .padding(.bottom, 20)
+                    
                     Button {
                         navigateFromRecycle = true
                     } label: {
@@ -28,7 +31,7 @@ struct RecycleView: View {
                     }
                     .padding(.horizontal)
                     .padding(.top, 20)
-
+                    
                     Section(header: SectionHeader(title: "Komunitas", linkTitle: "Lihat Semua")) {
                         VStack(spacing: 10) {
                             CommunityCard(imageName: "community1", title: "Bank Sampah Surabaya Induk", description: "Bank Sampah Induk Surabaya telah aktif berkecimpung dalam bidang pengelolaan sampah selama lebih dari 10 tahun.")
@@ -37,7 +40,7 @@ struct RecycleView: View {
                         }
                         .padding(.horizontal)
                     }
-
+                    
                     Section(header: SectionHeader(title: "Berita", linkTitle: "Lihat Semua")) {
                         VStack(spacing: 10) {
                             NewsCard(imageName: "news1", title: "Dampak Positif dari Konten Pandawara Grup, Anak-anak di Wakatobi Ini Terjun ke Sungai dan Membersihkan Sampah yang Menumpuk", date: "23 Juli 2024")
@@ -46,48 +49,53 @@ struct RecycleView: View {
                         }
                         .padding(.horizontal)
                     }
-
+                    
                     Spacer()
                 }
             }
             .navigationTitle("Recycle")
+            .navigationBarHidden(true)
             .navigationDestination(isPresented: $navigateFromRecycle) {
                 WasteCategoryView(navigateFromRecycle: $navigateFromRecycle)
             }
             .navigationDestination(isPresented: $navigateToProfile) {
                 ProfileView()
             }
-            .overlay(
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        navigateToProfile = true
-                    }) {
-                        ZStack {
-                            Circle()
-                                .fill(Color.white)
-                                .frame(width: 50, height: 50)
-                                .shadow(radius: 2)
-                        Image(systemName: "person.circle.fill")
-                            .resizable()
-                            .frame(width: 35, height: 35)
-                            .foregroundColor(.primary)
-                            .padding()
-                        }
-                    }
-                }
-//                    .frame(height: 0)
-//                .padding(.top, 0) // Adjust this value to move the button higher
-                , alignment: .topTrailing
-            )
         }
+    }
+}
+
+struct CustomHeaderView: View {
+    @Binding var navigateToProfile: Bool
+    
+    var body: some View {
+        HStack {
+            Text("Recycle")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.primary)
+            
+            Spacer()
+            
+            Button(action: {
+                navigateToProfile = true
+            }) {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 35, height: 35)
+                    .foregroundColor(.primary)
+                    .padding(5)
+            }
+            .background(Circle().fill(Color.white).shadow(radius: 2))
+        }
+        .padding()
     }
 }
 
 struct SectionHeader: View {
     var title: String
     var linkTitle: String
-
+    
     var body: some View {
         HStack {
             Text(title)
@@ -111,7 +119,7 @@ struct CommunityCard: View {
     var imageName: String
     var title: String
     var description: String
-
+    
     var body: some View {
         HStack(alignment: .top) {
             Image(imageName)
@@ -139,7 +147,7 @@ struct NewsCard: View {
     var imageName: String
     var title: String
     var date: String
-
+    
     var body: some View {
         HStack(alignment: .top) {
             Image(imageName)
--- Roambank/Views/ProfileView.swift
+++ Roambank/Views/ProfileView.swift
@@ -16,106 +16,102 @@ struct ProfileView: View {
     
     var body: some View {
         if !firstName.isEmpty && !email.isEmpty {
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text("Profile")
-                                .font(.largeTitle)
+            VStack {
+                HStack {
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack {
+                            Image(systemName: "person.crop.circle.fill")
+                                .resizable()
+                                .frame(width: 60, height: 60)
                                 .foregroundColor(Color("Ijo"))
-                                .bold()
                             
-                            HStack {
-                                Image(systemName: "person.crop.circle.fill")
-                                    .resizable()
-                                    .frame(width: 60, height: 60)
-                                    .foregroundColor(Color("Ijo"))
+                            VStack(alignment: .leading) {
+                                Text("\(firstName) \(lastName)")
+                                    .font(.title2)
+                                    .bold()
                                 
-                                VStack(alignment: .leading) {
-                                    Text("\(firstName) \(lastName)")
-                                        .font(.title2)
-                                        .bold()
-                                    
-                                    Text("\(email)")
-                                        .font(.subheadline)
-                                        .foregroundColor(.gray)
-                                }
-                                
-                                Spacer()
-                                
-                                Button(action: {
-
-                                }) {
-                                    Image(systemName: "arrow.right.circle")
-                                        .resizable()
-                                        .frame(width: 24, height: 24)
-                                        .foregroundColor(Color("Ijo"))
-                                }
-                            }
-                        }
-                        .padding()
-                        
-                        Spacer()
-                    }
-                    
-                    Form {
-                        Section(header: Text("Account Settings").foregroundColor(.gray)) {
-                            NavigationLink(destination: Text("Linked Accounts View")) {
-                                HStack {
-                                    Text("Linked Accounts")
-                                }
-                            }
-                            
-                            NavigationLink(destination: Text("Change Password View")) {
-                                HStack {
-                                    Text("Change Password")
-                                }
-                            }
-                            
-                            HStack {
-                                Text("Add Account Number")
-                                Spacer()
-                                Image(systemName: "plus.circle")
+                                Text("\(email)")
+                                    .font(.subheadline)
                                     .foregroundColor(.gray)
                             }
                             
-                            Toggle(isOn: $pushNotificationOn) {
-                                Text("Push Notifications")
-                            }
-                            .toggleStyle(SwitchToggleStyle(tint: Color("Ijo")))
+                            Spacer()
                             
-                            Toggle(isOn: $darkModeOn) {
-                                Text("Dark Mode")
-                            }
-                            .toggleStyle(SwitchToggleStyle(tint: Color("Ijo")))
-                        }
-                        
-                        Section(header: Text("More").foregroundColor(.gray)) {
-                            NavigationLink(destination: Text("About Us View")) {
-                                HStack {
-                                    Text("About Us")
-                                }
-                            }
-                            
-                            NavigationLink(destination: Text("Privacy Policy View")) {
-                                HStack {
-                                    Text("Privacy Policy")
-                                }
-                            }
-                            
-                            NavigationLink(destination: Text("Terms and Conditions View")) {
-                                HStack {
-                                    Text("Terms and Conditions")
-                                }
+                            Button(action: {
+                                
+                            }) {
+                                Image(systemName: "arrow.right.circle")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                    .foregroundColor(Color("Ijo"))
                             }
                         }
                     }
-                    .background(Color("LightGray"))
+                    .padding()
+                    
+                    Spacer()
                 }
-                .background(Color("LightGray").ignoresSafeArea())
-            
+                
+                Form {
+                    Section(header: Text("Account Settings").foregroundColor(.gray)) {
+                        NavigationLink(destination: Text("Linked Accounts View")) {
+                            HStack {
+                                Text("Linked Accounts")
+                            }
+                        }
+                        
+                        NavigationLink(destination: Text("Change Password View")) {
+                            HStack {
+                                Text("Change Password")
+                            }
+                        }
+                        
+                        HStack {
+                            Text("Add Account Number")
+                            Spacer()
+                            Image(systemName: "plus.circle")
+                                .foregroundColor(.gray)
+                        }
+                        
+                        Toggle(isOn: $pushNotificationOn) {
+                            Text("Push Notifications")
+                        }
+                        .toggleStyle(SwitchToggleStyle(tint: Color("Ijo")))
+                        
+                        Toggle(isOn: $darkModeOn) {
+                            Text("Dark Mode")
+                        }
+                        .toggleStyle(SwitchToggleStyle(tint: Color("Ijo")))
+                    }
+                    
+                    Section(header: Text("More").foregroundColor(.gray)) {
+                        NavigationLink(destination: Text("About Us View")) {
+                            HStack {
+                                Text("About Us")
+                            }
+                        }
+                        
+                        NavigationLink(destination: Text("Privacy Policy View")) {
+                            HStack {
+                                Text("Privacy Policy")
+                            }
+                        }
+                        
+                        NavigationLink(destination: Text("Terms and Conditions View")) {
+                            HStack {
+                                Text("Terms and Conditions")
+                            }
+                        }
+                    }
+                }
+                .background(Color("LightGray"))
+            }
+            .background(Color("LightGray").ignoresSafeArea())
+            .navigationTitle("Profile")
         } else {
             SignInWithAppleView()
         }
+        
     }
 }
 
