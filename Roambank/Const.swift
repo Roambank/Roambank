76878e5f5628696ddaae330a7382c6a1f1e9be2a
LisandraN08 <lnicoline@student.ciputra.ac.id>
2024-08-01 08:47:02 +0700

    feat: ganti baseURL Const

--- Roambank/Const.swift
+++ Roambank/Const.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Const {
-    static let baseURL = "http://lex.areveys.com/Roambank/api.php"
+    static let baseURL = "roambank.cisbyte.com/api.php"
     static let addUserEP = "addUser"
     static let addOrderEP = "addOrderWithWasteOrders"
     static let getUserBasedOnIdEP = "getUserBasedOnId"
